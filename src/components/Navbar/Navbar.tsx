f2350e8c1d746456f35f17dfef5ba4c3ae28b703
Jayganesh Gupta <jayganeshgupta0@gmail.com>
2025-10-17 22:41:30 +0530

    feat: make Navbar links navigable using useNavigate hook

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <div className="px-3 py-4 bg-black flex align-items-center gap-6 text-white">
       <h2 className="text-xl font-bold">React MCR</h2>
       <div className="flex align-items-center gap-1"></div>
-      <div className="">Home</div>
-      <div className="">About</div>
-      <div className="">Contact</div>
+      <div className="cursor-pointer" onClick={() => navigate("/")}>
+        Home
+      </div>
+      <div className="cursor-pointer">About</div>
+      <div className="cursor-pointer">Contact</div>
     </div>
   );
 };
